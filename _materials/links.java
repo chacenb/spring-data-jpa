8c6a97cbfeff07c5d148b09a47628e461d89960f
chace <chacenb@gmail.com>
2023-04-20 00:00:09 +0100

    links to materials

---
+++ _materials/links.java
@@ -0,0 +1,2 @@
+git link: /* https://github.com/amigoscode/spring-data-jpa-course */
+yt link:  /* https://www.youtube.com/watch?v=8SGI_XS5OPw&list=PLwvrYc43l1MzeA2bBYQhCWr2gvWLs9A7S&index=1&t=1302s */
